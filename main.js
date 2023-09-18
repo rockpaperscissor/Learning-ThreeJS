0e048a7d4c22b51a658cbb24fc55e010002927be
GAYATHRY P VASUDEVAN <84952234+rockpaperscissor@users.noreply.github.com>
2023-09-18 16:35:13 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,101 @@
+import './style.css'
+import gsap from "gsap"
+import * as THREE from 'three';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+//scene
+const scene = new THREE.Scene();
+
+//create your sphere
+const geometry = new THREE.SphereGeometry( 3,64,64)
+const material = new THREE.MeshStandardMaterial( 
+  { color: "#00ff83", 
+  roughness:0.5,
+} )
+const mesh = new THREE.Mesh( geometry, material )
+scene.add(mesh)
+//Sizes
+const sizes ={
+  width: window.innerWidth,
+  height: window.innerHeight,
+}
+
+//lights
+const light = new THREE.PointLight( 0xffffff, 100, 100 )
+const lightL = new THREE.AmbientLight( 0x404040 ); // soft white light
+
+light.position.set( 0, 10, 10 )
+light.intensity=1500
+scene.add( light )
+
+//camera
+const camera = new THREE.PerspectiveCamera(45, sizes.width/sizes.height,0.1,100)
+camera.position.z = 20
+scene.add(camera)
+
+
+
+//renderer
+const canvas = document.querySelector(".webgl")
+const  renderer =  new THREE.WebGLRenderer({canvas})
+renderer.setSize(sizes.width,sizes.height)
+renderer.setPixelRatio(5)
+renderer.render(scene,camera)
+
+//controls
+const controls = new OrbitControls(camera,canvas)
+controls.enableDamping = true
+controls.enablePan = false
+controls.enableZoom = false
+controls.autoRotate = true
+controls.autoRotateSpeed = 5
+
+
+//Resize
+window.addEventListener('resize', ()=> {
+  
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+  //update camers
+  
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+  renderer.setSize(sizes.width,sizes.height)
+})
+
+const loop = () => {
+  controls.update()
+  renderer.render(scene,camera)
+  window.requestAnimationFrame(loop)
+}
+loop()
+
+//gsap timeline magic
+const tl = gsap.timeline({defaults: { duration:1 }})
+tl.fromTo(mesh.scale, {z:0,x:0, y:0},{z:1, x:1, y:1})
+tl.fromTo("nav",{y:"-100%"},{y:"0%"})
+tl.fromTo(".title",{opacity:0},{opacity:1})
+
+//mouse animation color
+//change color only when dragging,no need hover
+let mouseDown = false
+let rgb = []
+window.addEventListener("mousedown", ()=> (mouseDown=true))
+window.addEventListener("mouseup",()=> (mouseDown = false))
+
+
+window.addEventListener("mousemove",(e) =>{
+  if(mouseDown){
+    rgb=[
+      Math.round((e.pageX /sizes.width)*255),Math.round((e.pageY /sizes.width)*255),100,
+    ]
+    //LETS ANIMATE
+    let newColor = new THREE.Color(`rgb(${rgb.join(",")})`)
+    
+    gsap.to(mesh.material.color,{
+      r:newColor.r,
+      g:newColor.g,
+      b:newColor.b
+    })
+
+  }
+})
